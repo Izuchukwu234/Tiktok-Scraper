1ab719de4d2f0caeb43970e1a0aefa7771df628b
Wisdom Adike <110326911+Izuchukwu234@users.noreply.github.com>
2025-05-11 22:01:00 +0100

    Update Twitter.py

--- pages/Twitter.py
+++ pages/Twitter.py
@@ -3,7 +3,12 @@ import streamlit as st
 st.set_page_config(page_title="Twitter Scraper | KOMI Group", page_icon="🐦")
 
 st.image("komi_logo.png", width=100)
-st.title("Twitter Scraper")
+st.markdown("""
+<div style="display: flex; align-items: center; gap: 10px;">
+    <img src="https://cdn-icons-png.flaticon.com/512/5968/5968958.png" width="36">
+    <h1 style="margin: 0;">Twitter Scraper</h1>
+</div>
+""", unsafe_allow_html=True)
 st.markdown("### COMING SOON!")
 st.markdown("---")
 
