5045c83e7271cf578980c280cd007943a506dd21
Wisdom Adike <110326911+Izuchukwu234@users.noreply.github.com>
2025-05-20 21:05:01 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,14 +1,18 @@
 import streamlit_authenticator as stauth
 import bcrypt
 
-usernames = ['KOMI']
-hashed_passwords = ['$2b$12$6lfyis7EL3QBVGhWBoGNk.ORiipCK3XQ5bmgR0cYRvjU5hK.4vWUy']
-
 def get_authenticator():
+    credentials = {
+        "usernames": {
+            "KOMI": {
+                "name": "Admin",
+                "password": "$2b$12$6lfyis7EL3QBVGhWBoGNk.ORiipCK3XQ5bmgR0cYRvjU5hK.4vWUy"
+            }
+        }
+    }
+
     return stauth.Authenticate(
-        names=['Admin'],
-        usernames=usernames,
-        passwords=hashed_passwords,
+        credentials=credentials,
         cookie_name='komi_app',
         key='auth',
         cookie_expiry_days=1
