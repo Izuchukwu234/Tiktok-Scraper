9e124879c53667db0b3ee0698ea83a36408d3884
Wisdom Adike <110326911+Izuchukwu234@users.noreply.github.com>
2025-05-21 18:39:41 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,19 +1,26 @@
 import streamlit_authenticator as stauth
-import bcrypt
 
 def get_authenticator():
     credentials = {
         "usernames": {
             "KOMI": {
                 "name": "Admin",
-                "password": "$2b$12$6lfyis7EL3QBVGhWBoGNk.ORiipCK3XQ5bmgR0cYRvjU5hK.4vWUy"
+                "password": "$2b$12$6lfyis7EL3QBVGhWBoGNk.ORiipCK3XQ5bmgR0cYRvjU5hK.4vWUy"  # bcrypt hash
             }
         }
     }
 
+    # Cookie configuration
+    cookie_config = {
+        'cookie_name': 'komi_radar_auth',
+        'key': '51d4f55920a4b81efb19ab690efce1dd',
+        'expiry_days': 1
+    }
+
     return stauth.Authenticate(
-        credentials,
-        cookie_name='komi_app',
-        key='auth',
-        cookie_expiry_days=1
+        credentials=credentials,
+        cookie_name=cookie_config['cookie_name'],
+        key=cookie_config['key'],
+        cookie_expiry_days=cookie_config['expiry_days'],
+        preauthorized=[]
     )
