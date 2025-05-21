921042207357699f6569eba0c55e3e41adfa41e0
Wisdom Adike <110326911+Izuchukwu234@users.noreply.github.com>
2025-05-21 20:09:50 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,3 +1,21 @@
+# import streamlit_authenticator as stauth
+
+# def get_authenticator():
+#     credentials = {
+#         "usernames": {
+#             "KOMI": {
+#                 "name": "Admin",
+#                 "password": "$2b$12$6lfyis7EL3QBVGhWBoGNk.ORiipCK3XQ5bmgR0cYRvjU5hK.4vWUy"
+#             }
+#         }
+#     }
+
+#     return stauth.Authenticate(
+#         credentials,
+#         cookie_name='komi_app',
+#         key='51d4f55920a4b81efb19ab690efce1dd',
+#         cookie_expiry_days=1
+#     )
 import streamlit_authenticator as stauth
 
 def get_authenticator():
@@ -10,9 +28,11 @@ def get_authenticator():
         }
     }
 
-    return stauth.Authenticate(
+    authenticator = stauth.Authenticate(
         credentials,
         cookie_name='komi_app',
-        key='51d4f55920a4b81efb19ab690efce1dd',
+        key='auth',
         cookie_expiry_days=1
     )
+
+    return authenticator, credentials
