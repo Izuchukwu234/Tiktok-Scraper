58fa4676dab885bef22e403f0274189417e5efcd
Wisdom Adike <110326911+Izuchukwu234@users.noreply.github.com>
2025-05-21 18:48:01 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -5,22 +5,14 @@ def get_authenticator():
         "usernames": {
             "KOMI": {
                 "name": "Admin",
-                "password": "$2b$12$6lfyis7EL3QBVGhWBoGNk.ORiipCK3XQ5bmgR0cYRvjU5hK.4vWUy"  # bcrypt hash
+                "password": "$2b$12$6lfyis7EL3QBVGhWBoGNk.ORiipCK3XQ5bmgR0cYRvjU5hK.4vWUy"
             }
         }
     }
 
-    # Cookie configuration
-    cookie_config = {
-        'cookie_name': 'komi_radar_auth',
-        'key': '51d4f55920a4b81efb19ab690efce1dd',
-        'expiry_days': 1
-    }
-
     return stauth.Authenticate(
-        credentials=credentials,
-        cookie_name=cookie_config['cookie_name'],
-        key=cookie_config['key'],
-        cookie_expiry_days=cookie_config['expiry_days'],
-        preauthorized=[]
+        credentials,
+        cookie_name='komi_app',
+        key='51d4f55920a4b81efb19ab690efce1dd',
+        cookie_expiry_days=1
     )
