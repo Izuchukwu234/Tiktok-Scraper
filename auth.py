c51e783f6ab06a3ba83e04a9c466739553aa1856
Wisdom Adike <110326911+Izuchukwu234@users.noreply.github.com>
2025-05-21 21:26:33 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,21 +1,3 @@
-# import streamlit_authenticator as stauth
-
-# def get_authenticator():
-#     credentials = {
-#         "usernames": {
-#             "KOMI": {
-#                 "name": "Admin",
-#                 "password": "$2b$12$6lfyis7EL3QBVGhWBoGNk.ORiipCK3XQ5bmgR0cYRvjU5hK.4vWUy"
-#             }
-#         }
-#     }
-
-#     return stauth.Authenticate(
-#         credentials,
-#         cookie_name='komi_app',
-#         key='51d4f55920a4b81efb19ab690efce1dd',
-#         cookie_expiry_days=1
-#     )
 import streamlit_authenticator as stauth
 
 def get_authenticator():
@@ -28,11 +10,29 @@ def get_authenticator():
         }
     }
 
-    authenticator = stauth.Authenticate(
+    return stauth.Authenticate(
         credentials,
         cookie_name='komi_app',
-        key='auth',
+        key='51d4f55920a4b81efb19ab690efce1dd',
         cookie_expiry_days=1
     )
+# import streamlit_authenticator as stauth
 
-    return authenticator, credentials
+# def get_authenticator():
+#     credentials = {
+#         "usernames": {
+#             "KOMI": {
+#                 "name": "Admin",
+#                 "password": "$2b$12$6lfyis7EL3QBVGhWBoGNk.ORiipCK3XQ5bmgR0cYRvjU5hK.4vWUy"
+#             }
+#         }
+#     }
+
+#     authenticator = stauth.Authenticate(
+#         credentials,
+#         cookie_name='komi_app',
+#         key='auth',
+#         cookie_expiry_days=1
+#     )
+
+#     return authenticator, credentials
