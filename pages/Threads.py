4c1546cea2518e8be75c5a88b1dd50d7d19d1017
Wisdom Adike <110326911+Izuchukwu234@users.noreply.github.com>
2025-05-24 00:06:49 +0100

    Update Threads.py

--- pages/Threads.py
+++ pages/Threads.py
@@ -1,7 +1,10 @@
 import streamlit as st
 from auth import get_authenticator
+from style import inject_custom_css
 
 st.set_page_config(page_title="Threads Scraper | KOMI Group", page_icon="💬")
+
+inject_custom_css()
 
 # --- AUTHENTICATION ---
 authenticator = get_authenticator()
