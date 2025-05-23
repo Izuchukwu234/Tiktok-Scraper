4e0198259d70958090ad9f670c1a788c42f878fd
Wisdom Adike <110326911+Izuchukwu234@users.noreply.github.com>
2025-05-23 23:30:15 +0100

    Update TikTok.py

--- pages/TikTok.py
+++ pages/TikTok.py
@@ -19,6 +19,8 @@ if not st.session_state.get("authentication_status"):
 
 # --- Show logout in sidebar ---
 authenticator.logout("Logout", location="sidebar")
+st.sidebar.markdown("<br><br>", unsafe_allow_html=True)
+st.sidebar.image("logo_2.png")
 
 # --- SETTINGS ---
 API_TOKEN = "lSNX5D8FW02vlTX4"
