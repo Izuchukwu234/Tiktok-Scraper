f30cfe777e14ac4338fde9e72e28201fe72ebed9
Wisdom Adike <110326911+Izuchukwu234@users.noreply.github.com>
2025-05-27 08:20:03 +0100

    Update How We Collect Data.py

--- pages/How We Collect Data.py
+++ pages/How We Collect Data.py
@@ -117,8 +117,8 @@ if not st.session_state.get("authentication_status"):
 
 # --- Show logout in sidebar ---
 authenticator.logout("Logout", location="sidebar")
-st.sidebar.markdown("<br><br>", unsafe_allow_html=True)
-st.sidebar.image("logo_2.png")
+# st.sidebar.markdown("<br>", unsafe_allow_html=True)
+# st.sidebar.image("logo_2.png")
 
 # Logo and title
 st.image("komi_logo.png", width=100)
